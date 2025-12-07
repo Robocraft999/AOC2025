ce58c9cb37b51ab254a89b6819b27d12bb226d1b
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2025-12-07 07:52:31 +0100

    day7 improved

--- day07/main.go
+++ day07/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strings"
 
 	"example.com/helper"
@@ -10,8 +9,7 @@ import (
 
 func main() {
 	input := helper.Input(7)
-	input = helper.TestInput(7, "testinput.txt")
-	//input = helper.TestInput(7, "testinput2.txt")
+	//input = helper.TestInput(7, "testinput.txt")
 	part1(input)
 	part2(input)
 }
@@ -22,28 +20,25 @@ func part1(input string) {
 	helper.PrintGrid(grid)
 
 	startIndex := strings.Index(lines[0], "S")
-	currentIndices := []int{startIndex}
-	nextIndices := make([]int, 0)
 	sum := 0
-	for y := range grid[1:] {
-		nextIndices = make([]int, 0)
-		for x := range grid[y] {
-			if slices.Contains(currentIndices, x) {
+	currentIndicesMap := make(map[int]int)
+	currentIndicesMap[startIndex] = 1
+	for y := range grid[1 : len(grid)-1] {
+		y += 1
+		for x, amt := range currentIndicesMap {
+			if amt > 0 {
 				if grid[y][x] == "^" {
+					currentIndicesMap[x] = 0
 					sum += 1
-					if x > 0 && !slices.Contains(nextIndices, x-1) {
-						nextIndices = append(nextIndices, x-1)
+					if x > 0 {
+						currentIndicesMap[x-1] += amt
 					}
-					if x < len(grid[y])-1 && !slices.Contains(nextIndices, x+1) {
-						nextIndices = append(nextIndices, x+1)
+					if x < len(grid[y])-1 {
+						currentIndicesMap[x+1] += amt
 					}
-				} else {
-					nextIndices = append(nextIndices, x)
 				}
 			}
 		}
-		fmt.Println(nextIndices)
-		currentIndices = nextIndices
 	}
 	fmt.Println(sum)
 }
@@ -53,40 +48,25 @@ func part2(input string) {
 	grid := helper.MakeGridFromLines(lines)
 
 	startIndex := strings.Index(lines[0], "S")
-	currentIndices := []int{startIndex}
-	nextIndices := make([]int, 0)
 	sum := 0
 	currentIndicesMap := make(map[int]int)
 	currentIndicesMap[startIndex] = 1
 	for y := range grid[1 : len(grid)-1] {
 		y += 1
-		nextIndices = make([]int, 0)
-		fmt.Println(grid[y], currentIndicesMap)
-		for _, x := range currentIndices {
-			if slices.Contains(currentIndices, x) {
+		for x, amt := range currentIndicesMap {
+			if amt > 0 {
 				if grid[y][x] == "^" {
-					amt := currentIndicesMap[x]
 					currentIndicesMap[x] = 0
 					if x > 0 {
-						if !slices.Contains(nextIndices, x-1) {
-							nextIndices = append(nextIndices, x-1)
-						}
 						currentIndicesMap[x-1] += amt
 					}
 					if x < len(grid[y])-1 {
-						if !slices.Contains(nextIndices, x+1) {
-							nextIndices = append(nextIndices, x+1)
-						}
 						currentIndicesMap[x+1] += amt
 					}
-				} else {
-					nextIndices = append(nextIndices, x)
 				}
 			}
 		}
-		currentIndices = nextIndices
 	}
-	fmt.Println(grid[len(grid)-1], currentIndicesMap)
 	for _, v := range currentIndicesMap {
 		sum += v
 	}
